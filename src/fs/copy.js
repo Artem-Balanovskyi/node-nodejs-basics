803f108e005c3348c4bfe55eefc6cd17643f5cad
Artem Balanovskyi <mr.balanovsky@gmail.com>
2022-12-02 20:54:32 +0200

    feat: add copy fn

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
-const copy = async () => {
-    // Write your code here 
+import { cp } from 'node:fs/promises';
+import { join } from 'path';
+import { FS_ERROR, FS_DIR } from './constants.js';
+
+export const copy = async () => {
+    const files_dir = join(FS_DIR, "/files/");
+    const files_copy_dir = join(FS_DIR, "/files_copy/");
+
+    try {
+        await cp(files_dir,files_copy_dir, {recursive: true, force: false, errorOnExist: true});
+    } catch (err) {
+        throw new Error(FS_ERROR);
+    }
+
 };
 
 copy();
