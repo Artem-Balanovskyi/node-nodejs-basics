ab4dabe649de66c0640ba7d36ea17138511c035c
Artem Balanovskyi <mr.balanovsky@gmail.com>
2022-12-03 21:17:30 +0200

    feat: add read fn

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'path';
+import { FS_FILES_DIR, ERROR_HANDLER } from './constants.js';
+
 const read = async () => {
-    // Write your code here 
+    const fileToReadDir = join(FS_FILES_DIR, "fileToRead.txt");
+
+    try {
+        const fileToReadContent = await readFile(fileToReadDir, { encoding: 'utf8' });
+        console.log(fileToReadContent);
+    } catch (err) {
+        ERROR_HANDLER();
+    }
 };
 
 await read();
