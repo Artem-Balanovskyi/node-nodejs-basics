98992b77cda21abf32b88d103d34b78ef4b068f8
Artem Balanovskyi <mr.balanovsky@gmail.com>
2022-12-03 21:11:08 +0200

    feat: add list fn

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import { readdir } from 'node:fs/promises';
+import { FS_FILES_DIR, ERROR_HANDLER } from './constants.js';
+
 const list = async () => {
-    // Write your code here 
+    try {
+        const allFiles = await readdir(FS_FILES_DIR);
+        console.log(allFiles);
+    } catch (err) {
+        ERROR_HANDLER();
+    }
 };
 
 await list();
