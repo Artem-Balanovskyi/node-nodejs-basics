a18752caae4584e33b4229fbbf6962b38d63a014
Artem Balanovskyi <mr.balanovsky@gmail.com>
2022-12-05 18:19:23 +0200

    feat: add child_process fn

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,21 @@
+import { fork } from 'child_process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const args = process.argv.slice(2);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const scriptFilePath = join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = fork(scriptFilePath, args, { silent: true });
+
+    process.stdin.on('data', (data) => {
+        childProcess.stdin.write(data.toString());
+    });
+
+    childProcess.stdout.on('data', (data) => {
+        process.stdout.write(data.toString());
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess(args);
